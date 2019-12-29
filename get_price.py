6d88ec69d901d293c046ecfa2fea81ff8e99207c
www10177 <www10177@gmail.com>
2019-12-29 17:04:13 +0800

    Add Login Session (from CLI)

--- get_price.py
+++ get_price.py
@@ -1,92 +1,134 @@
 import requests
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+import requests
+import browser_cookie3
+import steam.webauth as wa
+def get_session():
+    username = input('Please Enter User Name')
+    password = input('Please Enter Password')
+    captcha=None
+    twofactor=None
+    print(username,password)
+    while True:
+        try:
+            user = wa.WebAuth(username,password)
+        except (wa.CaptchaRequired, wa.LoginIncorrect) as exp:
+            print(exp)
+            if isinstance(exp, LoginIncorrect):
+                password = input('Please Enter Correct Password')
+            if isinstance(exp, wa.CaptchaRequired):
+                print('Please Solve Captcha')
+                print (user.captcha_url)
+            else:
+                print('other exceptiion')
+                captcha = None
+        else:
+            break
+    while True:
+        twofactor =  input('Please Enter TwoFactorCode')
+        user.login(twofactor_code= twofactor)
+        break
+        try:
+            user.login(twofactor_code= twofactor)
+        except wa.TwoFactorCodeRequired:
+            twofactor = input('Please Enter TwoFactorCode')
+        except exceptiion as e:
+            print(e)
+        else:
+            print('Login Success')
+            break
+    return user.session
+
 
 def strip_price(s):
-	s = s.strip()
-	s = s.replace(',','')
-	s= s.replace('NT$','')
-	return float(s)
-def get_price(appid):
+    s = s.strip()
+    s = s.replace(',','')
+    s= s.replace('NT$','')
+    return float(s)
+def get_price(appid,sess):
 
+    #Url Setup
+    exchange_url  ='https://www.steamcardexchange.net/index.php?gamepage-appid-{appid}'
+    ua = UserAgent()
+    headers = {'User-Agent': ua.chrome}
+    #Requests
+    req = requests.get(exchange_url.format(appid=appid),headers=headers)
 
-	#Url Setup
-	exchange_url  ='https://www.steamcardexchange.net/index.php?gamepage-appid-{appid}'
-	ua = UserAgent()
-	headers = {'User-Agent': ua.random}
-	#Requests
-	req = requests.get(exchange_url.format(appid=appid),headers=headers)
+    #Parse HTML
+    card_link_list = []
 
-	#Parse HTML
-	card_link_list = []
+    #Get All Card link list fomr steamcardexchange
+    soup = BeautifulSoup(req.text,'html.parser')
+    for showcase in soup.find_all('div',class_='showcase-element-container card'):
+        for card in showcase.find_all('div',class_='element-button'):
+            for a in card.find_all('a'):
+                card_link_list.append(a.get('href'))
 
-	#Get All Card link list fomr steamcardexchange
-	soup = BeautifulSoup(req.text,'html.parser')
-	for showcase in soup.find_all('div',class_='showcase-element-container card'):
-		for card in showcase.find_all('div',class_='element-button'):
-			for a in card.find_all('a'):
-				card_link_list.append(a.get('href'))
+    #Get Card Price from steam market
+    currency_str = '/render?start=0&count=10&currency=30&format=json'
+    price_list = []
+    #print(card_link_list)
 
-	#Get Card Price from steam market
-	currency_str = '/render?start=0&count=10&currency=30&format=json'
-	price_list = []
-	#print(card_link_list)
+    for link in card_link_list:
+        marketname = link.split('/')[-1]
+        d = {'name':marketname,'link':link}
+        print(link+currency_str)
+        req  = sess.get(link+currency_str,headers=headers)
+        json = req.json()
+        if 'Foil' in marketname:
+            '''
+            d['sell_price_with_fee'] = -1
+            d['sell_price_without_fee'] = -1
+            price_list.append(d)
 
-	for link in card_link_list:
-		marketname = link.split('/')[-1]
-		d = {'name':marketname,'link':link}
-		print(link+currency_str)
-		req  = requests.get(link+currency_str,headers=headers)
-		json = req.json()
-		if 'Foil' in marketname:
-			d['sell_price_with_fee'] = -1
-			d['sell_price_without_fee'] = -1
-			price_list.append(d)
-			continue
-		#print(json)
-		try :
-			html = json['results_html']
-			soup = BeautifulSoup(html,'html.parser')
-			price = soup.find('span',class_='market_listing_price market_listing_price_with_fee')
-			d['sell_price_with_fee'] = strip_price(price.get_text())
-			price = soup.find('span',class_='market_listing_price market_listing_price_without_fee')
-			d['sell_price_without_fee'] = strip_price(price.get_text())
-		except Exception as e:
-			print(e)
-			print('GET_PRICE:link parse except')
-			d['sell_price_with_fee'] = -1
-			d['sell_price_without_fee'] = -1
-		price_list.append(d)
+            continue
+            '''
+            pass
+        #print(json)
+        try :
+            html = json['results_html']
+            soup = BeautifulSoup(html,'html.parser')
+            price = soup.find('span',class_='market_listing_price market_listing_price_with_fee')
+            d['sell_price_with_fee'] = strip_price(price.get_text())
+            price = soup.find('span',class_='market_listing_price market_listing_price_without_fee')
+            d['sell_price_without_fee'] = strip_price(price.get_text())
+        except Exception as e:
+            print(e)
+            print('GET_PRICE:link parse except')
+            d['sell_price_with_fee'] = -1
+            d['sell_price_without_fee'] = -1
+        price_list.append(d)
 
-		#Get 24hr Volume
-		#req  = requests.get(link,headers=headers)
-		#soup = BeautifulSoup(req.text,'html.parser')
-		#for div in soup.find_all('div',class_='es_sold_amount'):
-		#	print(div)
-	return price_list
+        #Get 24hr Volume
+        #req  = requests.get(link,headers=headers)
+        #soup = BeautifulSoup(req.text,'html.parser')
+        #for div in soup.find_all('div',class_='es_sold_amount'):
+        #   print(div)
+    return price_list
 def cal_avg(price_list) :
-	#Input: price_list from get_price
-	#Return:price_tuple(with fee,without fee) (Sum of not foil card only)
-	price_list = [i for i in price_list if 'Foil' not in i['name']]
-	without_fee,with_fee = 0.0,0.0
-	for item in price_list:
-		with_fee+= item['sell_price_with_fee']
-		without_fee += item['sell_price_without_fee']
-	l = len(price_list)
-	return without_fee/l
+    #Input: price_list from get_price
+    #Return:price_tuple(with fee,without fee) (Sum of not foil card only)
+    price_list = [i for i in price_list if 'Foil' not in i['name']]
+    without_fee,with_fee = 0.0,0.0
+    for item in price_list:
+        with_fee+= item['sell_price_with_fee']
+        without_fee += item['sell_price_without_fee']
+    l = len(price_list)
+    return without_fee/l
 
-def get_gem_price():
-	#Return Gem price(float)
-	try :
-		ua = UserAgent()
-		headers = {'User-Agent': ua.random}
-		url = r'https://steamcommunity.com/market/listings/753/753-Sack%20of%20Gems/render?start=0&count=10&currency=30&format=json'
-		req  = requests.get(url,headers=headers)
-		html = req.json()['results_html']
-		soup = BeautifulSoup(html,'html.parser')
-		price = soup.find('span',class_='market_listing_price market_listing_price_with_fee')
-		return strip_price(price.get_text())
-	except Exception as e :
-		print(e)
-		return -1
+def get_gem_price(sess):
+    #Return Gem price(float)
+    try :
+        ua = UserAgent()
+        headers = {'User-Agent': ua.chrome}
+        url = r'https://steamcommunity.com/market/listings/753/753-Sack%20of%20Gems/render?start=0&count=10&currency=30&format=json'
+        req  = sess.get(url,headers=headers)
+        html = req.json()['results_html']
+        soup = BeautifulSoup(html,'html.parser')
+        price = soup.find('span',class_='market_listing_price market_listing_price_with_fee')
+        return strip_price(price.get_text())
+    except Exception as e :
+        print(e)
+        return -1
 
--- web.py
+++ web.py
@@ -2,19 +2,25 @@ from get_price import *
 from flask import Flask,render_template,request
 import sys
 import os
-
+sess =None
 app=Flask(__name__)
 @app.route('/',methods=['GET'])
 def home():
+	global sess
+	if sess is None :
+		sess= get_session()
 	return render_template('home.html')
 
 @app.route('/results',methods=['GET','POST'])
 def results():
+	global sess
+	if sess is None :
+		sess= get_session()
 	appid=request.values['appid']
-	price_list = get_price(appid)
+	price_list = get_price(appid,sess)
 	print(price_list)
 	card_avg = '{:.02f}'.format(cal_avg(price_list))
-	gem_price = '{:.02f}'.format(get_gem_price())
+	gem_price = '{:.02f}'.format(get_gem_price(sess))
 	return render_template('result.html',appid=appid, 
 		price_list = price_list, card_avg = card_avg, gem_price = gem_price)
 
